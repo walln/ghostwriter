02e966c25a7d23013f207a4a5df54953a4092450
Nick Wall <46641379+walln@users.noreply.github.com>
2024-05-08 15:32:34 -0500

    chore: docstrings

--- src/ghostwriter/dataset/scrape_lyrics.py
+++ src/ghostwriter/dataset/scrape_lyrics.py
@@ -16,49 +16,76 @@ console = Console()
 
 auth_token = os.getenv("GENIUS_API_TOKEN")
 assert auth_token is not None, "Missing GENIUS_API_TOKEN"
+genius = lyricsgenius.Genius(auth_token)
 
 
-def _get_lyrics_for_songs(songs: list[dict]):
+def create_row(
+    song: dict, *, lyrics: str | None = None, error: str | None = None
+) -> dict:
+    """Create a row for the dataset.
+
+    Args:
+        song: The song information.
+        lyrics: The lyrics for the song.
+        error: The error message if the lyrics could not be found.
+
+    Returns:
+        A dictionary containing the song information.
+    """
+    return {
+        "song_id": song["song_id"],
+        "song_name": song["song_name"],
+        "lyrics": lyrics if lyrics else "NONE",
+        "lyrics_path": song["lyrics_path"],
+        "error": error if error else "NONE",
+    }
+
+
+# TODO: docstrings and public functions
+def get_lyrics_for_songs(songs: list[dict]):
+    """For each song in the list, get the lyrics from the Genius API.
+
+    This is a generator function that yields the song information for each
+    song. This is because the song list can be large and because scraping the
+    lyrics for each song is IO-bound, it is more efficient to yield the results
+    so that the dataset builder can parallelize this process across multiple
+    songs at once.
+
+    Args:
+        songs: A list of songs to get the lyrics for.
+
+    Returns:
+        A list of dictionaries containing the song information.
+    """
     for song in songs:
         if song["lyrics_state"] != "complete":
             continue
         else:
             try:
-                genius = lyricsgenius.Genius(auth_token)
-                song_url = "https://genius.com" + song["lyrics_path"]
-                res = genius.lyrics(song_url=song_url)
-
-                if not res:
-                    yield {
-                        "error": "lyrics not found",
-                        "song_id": song["song_id"],
-                        "song_name": song["song_name"],
-                        "lyrics_path": song["lyrics_path"],
-                        "lyrics": "NONE",
-                    }
-
-                else:
-                    # Drop everything before the first line break
-                    res = res[res.find("\n") + 1 :]
-
-                    yield {
-                        "song_id": song["song_id"],
-                        "song_name": song["song_name"],
-                        "lyrics": res,
-                        "lyrics_path": song["lyrics_path"],
-                        "error": "NONE",
-                    }
+                res = genius.lyrics(song_url="https://genius.com" + song["lyrics_path"])
+                yield (
+                    create_row(song, error="lyrics not found")
+                    if not res
+                    else create_row(song, lyrics=res[res.find("\n") + 1 :])
+                )
             except Exception as e:
-                yield {
-                    "error": str(e),
-                    "song_id": song["song_id"],
-                    "song_name": song["song_name"],
-                    "lyrics_path": song["lyrics_path"],
-                    "lyrics": "NONE",
-                }
+                yield create_row(song, error=str(e))
 
 
-def _get_songs_for_artist(artist_id: str) -> list:
+def get_songs_for_artist(artist_id: str) -> list[dict]:
+    """Get the songs for an artist from the Genius API.
+
+    Searches for all songs where the supplied artist is the primary
+    artist on the song. The song information is returned as a list of
+    dictionaries. Each dictionary contains the song ID, song name,
+    lyrics state, and lyrics path.
+
+    Args:
+        artist_id: The ID of the artist.
+
+    Returns:
+    A list of songs for the artist.
+    """
     url = f"https://genius.com/api/artists/{artist_id}/songs"
 
     table = Table(title="Songs")
@@ -116,7 +143,19 @@ def _get_songs_for_artist(artist_id: str) -> list:
             raise ValueError("Failed to get lyrics")
 
 
-def _get_artist_information(artist_name: str) -> dict:
+def get_artist_information(artist_name: str) -> dict:
+    """Get the artist information from the Genius API.
+
+    Searches for the artist with the given name and returns the artist ID and name.
+    This function is interactive and will prompt the user to select the correct artist as it
+    iteratively searches for the artist corresponding to the given name.
+
+    Args:
+        artist_name: The name of the artist to search for.
+
+    Returns:
+        A dictionary containing the artist ID and name.
+    """
     table = Table(title="Artist")
     table.add_column("Number", style="cyan", no_wrap=True)
     table.add_column("Artist Name", style="magenta")
